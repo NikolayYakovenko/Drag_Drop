a73fc573391dcbf41d2fdc508bdc9e5cffad53a1
n.yakovenko <n.yakovenko@smartweb.com.ua>
2017-04-06 15:00:25 +0300

    added simple layout

--- react-app/layout/layout.js
+++ react-app/layout/layout.js
@@ -8,9 +8,19 @@ export const Layout = () => {
         <div>
             <Header text='asssdasdasd' />
             <div className="content-wrapper">
-                <div className="right-menu">qqqqq</div>
-                <div className="left-menu">wwwwwwww</div>
-                <div>
+                <div className="right-sidebar">
+                </div>
+                <div className="left-sidebar">
+                    <button className="button">+ Add</button>
+                    <ul className="left-menu">
+                        <li className="left-menu__item">Item 1</li>
+                        <li className="left-menu__item">Item 2</li>
+                        <li className="left-menu__item">Item 3</li>
+                        <li className="left-menu__item">Item 4</li>
+                        <li className="left-menu__item">Item 5</li>
+                    </ul>
+                </div>
+                <div className="main-content">
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus ut, dolor debitis cum atque tenetur numquam nisi sapiente ipsum quo perferendis reprehenderit eaque iusto eius excepturi. Modi enim at esse.
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, est iusto ipsum architecto magni temporibus aliquid veniam a fuga harum, facilis delectus nisi quaerat unde, nesciunt, provident inventore! Officia, quae.
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus ut, dolor debitis cum atque tenetur numquam nisi sapiente ipsum quo perferendis reprehenderit eaque iusto eius excepturi. Modi enim at esse.
